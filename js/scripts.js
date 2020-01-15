41da65df38e2667d24c8b30d8c8b20f59d3554cf
Christine Augustine and Joseph Wangemann <christine+joseph@>
2020-01-15 15:08:53 -0800

    add drop down menu and gradient background

--- js/scripts.js
+++ js/scripts.js
@@ -1,5 +1,9 @@
 $(document).ready(function() {
-    $("btn").click(function)
+$(".btn").click(function() {
     var beverage = $("#beverage").val();
-
-}
+    $("#output").append(beverage);
+    $("#output").show();
+    
+    event.preventDefault();
+    });
+  });
